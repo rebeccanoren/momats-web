0c037b0fa2601848e2c0abb1c9b0e2c332943d0f
Rebecca Norén <rebecca.noren@iteam.se>
2020-08-31 08:48:49 +0200

    added custom slice

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">Momatz Fysio</footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style lang="sass" scoped>
+.footer
+  max-width: 80%
+  padding: 40px 0
+  margin: 0 auto
+  text-align: center
+  border-top: 1px solid #DADADA
+
+.logo
+  width: 100px
+</style>
---
+++ components/Nav.vue
@@ -0,0 +1,96 @@
+<template>
+  <nav class="site-header">
+    <nuxt-link to="/">
+      <h1>{{ $prismic.asText(menu_title) }}</h1>
+      <h4>Momatz Fysio</h4>
+    </nuxt-link>
+    <ul class="menu">
+      <li v-for="link in menu_links" v-bind:key="link.label">
+        <prismic-link :field="link.link">{{ $prismic.asText(link.label) }}</prismic-link>
+      </li>
+    </ul>
+    <ul style="list-style-type: none;">
+      <li>Tjänster</li>
+      <li>Om oss</li>
+      <li>Kontakt</li>
+    </ul>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Nav",
+  props: {
+    menu_links: Array,
+    menu_title: Array,
+  },
+};
+</script>
+
+<style lang="sass">
+.site-header
+  max-width: 1200px
+  margin: 0 auto
+  padding: 20px 60px
+  color: #484d52
+  font-weight: 700
+  display: flex
+  justify-content: space-between
+
+  ul
+    display: flex
+
+  li
+    margin-left: 10px
+
+  a
+    color: #484d52
+    font-weight: 700
+  nav a:hover
+    color: #72767B
+
+.site-header
+  .logo
+    display: inline-block
+    font-size: 22px
+    font-weight: 900
+
+  nav
+    float: right
+
+    ul
+      margin: 0
+      padding-left: 0
+
+    li
+      display: inline-block
+      margin-left: 40px
+      list-style-type: none
+
+@media (max-width: 1060px)
+  .site-header
+    padding-left: 20px
+    padding-right: 20px
+
+@media (max-width: 767px)
+  .site-header
+    height: auto
+
+  .homepage .site-header
+    position: absolute
+    left: 0
+    right: 0
+
+  .site-header
+    .logo
+      display: block
+      text-align: center
+    nav
+      float: none
+      text-align: center
+      li
+        display: inline-block
+        margin-left: 10px
+        margin-right: 10px
+</style>
+
+
