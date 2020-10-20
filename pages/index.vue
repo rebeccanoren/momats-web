4c58ab5b0e7b6ef99972c5fef083658b63bf388c
Rebecca Norén <rebecca.noren@iteam.se>
2020-10-20 21:43:13 +0200

    updated copy

--- pages/index.vue
+++ pages/index.vue
@@ -13,12 +13,10 @@
           <img src="../assets/images/formthotics.jpg" alt="" />
           <h2>Ortopediska inlägg</h2>
           <p>
-            Har du ont i knän, ryggen, höfter eller överpronerar med fötterna?
-            En enkel och effektiv lösning kan då vara ortopediska inlägg från
-            Formthotics. Med inlägg i skorna formade efter hur du och din kropp
-            rör dig kan du få ett bättre stöd för dina fötter till ett bra pris.
-            Kontakta oss och boka en tid så kan du åka hem samma dag med dina
-            nya inlägg.
+            Har du ont i knän, ryggen, höfter eller fötterna? En effektiv
+            lösning är Formthotics ortopediska inlägg från. Vi formgjuter inlägg
+            som passar dina fötter och din kropp. Du får ett bra stöd till ett
+            bra pris.
           </p>
 
           <nuxt-link class="inline-block mt-5 mb-5 flex" to="/lopcoachning"
@@ -30,20 +28,17 @@
               8.5c0-.336.302-.608.674-.608h10.025L7.536 5.038a.567.567 0
               010-.86A.711.711 0 018.012 4c.173 0 .345.06.477.178l4.314
               3.892a.567.567 0 010 .86z"
-                fill="#06B957"
               />
             </svg>
           </nuxt-link>
         </div>
         <div class="card">
           <img src="../assets/images/running.jpg" alt="" />
-          <h2>Löpcoach</h2>
+          <h2>Löpcoachning</h2>
           <p>
             Vi erbjuder individuell löpcoaching, löpträning i grupp och för
-            företag. Vi fokuserar på en skonsam och effektiv löpteknik och kan
-            hjälpa dig med löpstegsteknik, skräddarsydda träningsscheman. Men
-            även hur du hittar tillbaka till glädjen i löpningen. Vi skräddarsyr
-            ditt träningsupplägg efter dina behov.
+            företag. Vi erbjuder löpcoachning, lär ut effektiv löpstegsteknik
+            och hjälper dig att hitta glädje i din träning.
           </p>
           <nuxt-link class="inline-block mt-5 mb-5 flex" to="/lopcoachning"
             >Mer om löpcoachning
@@ -54,7 +49,6 @@
               8.5c0-.336.302-.608.674-.608h10.025L7.536 5.038a.567.567 0
               010-.86A.711.711 0 018.012 4c.173 0 .345.06.477.178l4.314
               3.892a.567.567 0 010 .86z"
-                fill="#06B957"
               />
             </svg>
           </nuxt-link>
